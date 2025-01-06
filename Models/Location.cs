86ad9926cb522169ecf8466fe16fa3dfaf750626
mattswedin <swedin.matthew@gmail.com>
2025-01-06 16:50:55 -0500

    implement Models, Web API call, OpenWeather API call

---
+++ Models/Location.cs
@@ -0,0 +1,8 @@
+namespace UtilityBlazorApp.Models
+{
+    public class Location
+    {
+        public double lat { get; set; }
+        public double lon { get; set; }
+    }
+}
---
+++ Models/WeatherDetails.cs
@@ -0,0 +1,19 @@
+namespace UtilityBlazorApp.Models
+{
+    public class WeatherDetails
+    {
+        //"main" is a reference from the OpenWeather API JSON response
+        public Tempature? Main { get; set; }
+        public List<WeatherCondition>? Weather { get; set; }
+    }
+
+    public class Tempature
+    {
+        public double? Temp { get; set; }
+    }
+
+    public class WeatherCondition
+    {
+        public string? Main { get; set; }
+    }
+}
